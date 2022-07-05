aca1cbdb3d783fb05057d196536afac67d995a7e
康任均 <kangrenjun@kangrenjundeMacBook-Air.local>
2022-07-05 23:23:44 +0800

    2022/07/05 update02 No14 js

--- No14.js
+++ No14.js
@@ -3,5 +3,18 @@
  * @return {string}
  */
  var longestCommonPrefix = function(strs) {
-    
+    if(strs.length == 0) return "";
+
+    let pre = strs[0];
+    for(let i = 1; i < strs.length; i++)
+    {
+        while(strs[i].indexOf(pre) !== 0)
+        {
+            pre = pre.slice(0, pre.length - 1);
+            // console.log(pre);
+        }
+    }
+    return pre;
 };
+let s = ["cc","cacc","ccc"];
+console.log(longestCommonPrefix(s));
